68e9d2399d8674279378ec3230f3d83eab058954
Daffa Althaf Ridovani <daffaalthaf25@gmail.com>
2026-04-06 16:35:26 +0700

    feat: redirect if signed in

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,11 +1,17 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 import SignInForm from "@/components/auth/sign-in/SignInForm";
 import AuthHeader from "@/components/auth/AuthHeader";
 import Link from "next/link";
 
 export default async function SignInPage() {
+  const { isAuthenticated } = await auth();
+
+  if (isAuthenticated) redirect("/");
+
   return (
     <section className="bg-background flex min-h-screen relative overflow-hidden py-12 lg:px-28">
-      <main className="bg-card relative flex lg:w-[45%] flex-col items-center rounded-2xl justify-center  p-8 shadow-xl lg:px-24">
+      <main className="bg-card relative flex lg:w-[45%] flex-col items-center rounded-2xl justify-center p-8 shadow-xl lg:px-24">
         <AuthHeader
           title="Sign In Now!"
           subtitle="Sebelum melangkah lebih lanjut, silahkan masuk terlebih dahulu!"
